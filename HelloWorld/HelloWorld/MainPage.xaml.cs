d1bd3c3a2c0905c33ae51c4861aa5122eae8253d
Student <student@Mac.local>
2024-01-16 09:50:49 -0800

    Im a King

---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace HelloWorld;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		if(lblDisplay.Text == "Hello World")
+		{
+			lblDisplay.Text = "Good Day";
+		}
+		else
+		{
+			lblDisplay.Text = "Hello World";
+		}
+	}
+}
+
+

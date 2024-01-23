28ccdaf7734d72585052be48bea079c8bb35b1d5
Student <student@Mac.local>
2024-01-23 08:45:59 -0800

    git clone https://github.com/JBStrange1/MobileDev.git

---
+++ JBS.Calculator/JBS.Calculator/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace JBS.Calculator;
+
+public partial class MainPage : ContentPage
+{ 
+	public MainPage()
+	{
+		InitializeComponent();
+		Title = "Add Master Pro";
+	}
+
+    void Add_OnClick(System.Object sender, System.EventArgs e)
+    {
+        double firstNum; 
+        double secondNum;
+        double.TryParse(txtFirstNum.Text, out firstNum);
+        double.TryParse(txtSecondNum.Text, out secondNum);
+        double answer = firstNum + secondNum;
+        lblDisplay.Text = answer.ToString();
+    }
+
+    void Clear_OnClick(System.Object sender, System.EventArgs e)
+    {
+        txtFirstNum.Text = "";
+        txtSecondNum.Text = "";
+        lblDisplay.Text = "0";
+    }
+}
+
+
